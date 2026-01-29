6631bdff144075e789425f603f458b68c7476185
Ethan Gruening <ethgru@iastate.edu>
2026-01-28 22:32:07 -0600

    Dashboard with timeframe can be selected for any stock. Limit increased for API calls

--- stockai-web/src/components/StockSearch.tsx
+++ stockai-web/src/components/StockSearch.tsx
@@ -7,20 +7,14 @@ import {type ReactElement, useState} from "react";
 import Box from "@mui/material/Box";
 import {List, Typography } from "@mui/material";
 import ListItemButton from "@mui/material/ListItemButton";
-import type {LineSeries} from "@mui/x-charts/LineChart";
-import EarningsScatterChart, {type EarningsScatterChartProps} from "./EarningsScatterChart.tsx";
+import type {Bar} from "../services/api.ts";
 
-function getDateFromYYYYMMDD(yyyymmdd: string): string {
-    const year = parseInt(yyyymmdd.substring(0, 4), 10);
-    const month = parseInt(yyyymmdd.substring(4, 6), 10) - 1;
-    const day = parseInt(yyyymmdd.substring(6, 8), 10);
 
-    return new Date(Date.UTC(year, month, day)).toLocaleDateString();
+export interface StockSearchProps {
+    setData: (data: Bar[]) => void
 }
-
-export default function StockSearch() {
+export default function StockSearch(props: StockSearchProps) {
     const [searchSymbols, setSearchSymbols] = useState<Array<string>>([]);
-    const [data, setData] = useState<EarningsScatterChartProps>(null as unknown as EarningsScatterChartProps);
 
     function autoCompleteDropdown(symbols: Array<string>) {
         if(searchSymbols.length == 0) {
@@ -43,44 +37,12 @@ export default function StockSearch() {
         </List>
     }
 
-    function getDataTable(series: EarningsScatterChartProps) {
-        if (series === null) {
-            return <></>
-        }
-
-        return <EarningsScatterChart {...series}/>
-    }
 
     async function getSymbolData(symbol: string) {
         console.log("Fetching data for symbol: " + symbol)
         setSearchSymbols([])
         const response = await fetchStockHistory(symbol, "daily")
-
-        const data: number[] = []
-        const x_axis: string[] = []
-        response.results.forEach((item: any) => {
-            data.push(item.open)
-            x_axis.push(item.date)
-        })
-        setData({
-            series: [
-                {
-                    id: symbol,
-                    label: symbol,
-                    showMark: false,
-                    curve: 'linear',
-                    stack: 'total',
-                    area: true,
-                    stackOrder: 'ascending',
-                    data: data,
-                } as LineSeries
-            ],
-            x_axis: x_axis,
-            heading: `Stock Prices for ${symbol}`,
-            percent: 0,
-        })
-        console.log(data)
-        console.log(x_axis)
+        props.setData(response.results)
 
     }
 
@@ -109,7 +71,7 @@ export default function StockSearch() {
                         const result = await fetchStockSymbols(e.target.value, 5)
 
                         const completeSet : string[] = []
-                        result['results'].forEach((item: any) => {
+                        result['results'].forEach((item: never) => {
                             completeSet.push(item['symbol'])
                         })
                         setSearchSymbols(completeSet)
@@ -121,7 +83,7 @@ export default function StockSearch() {
                         const result = await fetchStockSymbols(e.target.value, 5)
 
                         const completeSet : string[] = []
-                        result['results'].forEach((item: any) => {
+                        result['results'].forEach((item: never) => {
                             completeSet.push(item['symbol'])
                         })
                         setSearchSymbols(completeSet)
@@ -134,7 +96,6 @@ export default function StockSearch() {
             </FormControl>
             {autoCompleteDropdown(searchSymbols)}
             <br/>
-            {getDataTable(data)}
         </>
 
     );
